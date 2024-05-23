f88a4654884fb1430fdc45b6b3e12b433379b325
JoDcntrl <jodcntrl28@gmail.com>
2024-05-23 10:31:33 +0300

    DCNTRL-18 fix fixed the CompanyEdit and CompanyCreation components

--- src/components/CompanyEdit/CompanyEdit.tsx
+++ src/components/CompanyEdit/CompanyEdit.tsx
@@ -13,7 +13,7 @@ import {
   useForm,
 } from "react-hook-form";
 import { useRouter } from "next/navigation";
-import { useTonWallet } from "@tonconnect/ui-react";
+import { useIsConnectionRestored, useTonWallet } from "@tonconnect/ui-react";
 
 import Input from "@/components/Input/Input";
 import Select from "@/components/Select/Select";
@@ -37,8 +37,11 @@ const CompanyEdit: React.FC = () => {
 
   const [activeQuestion, setActiveQuestion] = useState<boolean>(false);
 
-  const router = useRouter();
+  const [isWalletLoaded, setIsWalletLoaded] = useState<boolean>(false);
+
   const wallet = useTonWallet();
+  const router = useRouter();
+  const connectionRestored = useIsConnectionRestored();
 
   const {
     register,
@@ -69,10 +72,14 @@ const CompanyEdit: React.FC = () => {
   const linkLogoValue = watch("linkLogo");
 
   useEffect(() => {
-    if(!wallet) {
-    router.push('/');
-  }
-  },[])
+    if(connectionRestored) {
+      if(!wallet) {
+        router.push('/')
+      } else {
+        setIsWalletLoaded(true)
+      }
+    }    
+  },[connectionRestored,wallet]);
   
 
   useEffect(() => {
@@ -119,219 +126,226 @@ const CompanyEdit: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit, error)} className={styles.canvas}>
-      <div className={styles.canvasWrapper}>
-        <div className={styles.wrapperBlockLinks}>
-          <Link className={styles.blockLink} href="/">
-            Main
-          </Link>
-          <span className={styles.blockSlash}>/</span>
-          <Link className={styles.blockLink} href="/companies">
-            Companies
-          </Link>
-          <span className={styles.blockSlash}>/</span>
-          <Link className={styles.blockLink} href="/company-edit">
-            Stellar
-          </Link>
-          <span className={styles.blockSlash}>/</span>
-          <Link className={styles.blockLinkCurrent} href="/company-edit">
-            Edit
-          </Link>
-        </div>
-        <h1 className={styles.blockTitle}>
-          <span className={styles.blockTitleText}>Editing</span>
-          <Button
-            appearance="primary"
-            size="l"
-            type="submit"
-            className={styles.buttonStyle}
-          >
-            Save changes
-          </Button>
-        </h1>
-        <div className={styles.wrapperBlockMain}>
-          <main className={styles.main}>
-            <section className={styles.blockBasicInformation}>
-              <h2 className={styles.informationTitle}>Basic information</h2>
-              <div className={styles.informationName}>
-                <span className={styles.nameTitle}>Company name</span>
-                <div className={styles.informationInput}>
-                  <Input<CompanyEditFormTypes>
-                    name="companyName"
-                    placeholder="Stellar"
-                    register={register}
-                    error={errors.companyName}
-                  />
-                </div>
-              </div>
-              <div className={styles.informationSelect}>
-                <span className={styles.selectTitle}>Industry</span>
-                <div className={styles.selectInput}>
-                  <Controller
-                    name="industry"
-                    control={control}
-                    render={({ field: { onChange, value } }) => {
-                      return (
-                        <div>
-                          <Select
-                            color="#1B1E27"
-                            onChange={onChange}
-                            objValue={value}
-                            data={industry}
-                            placeholder="IT"
-                          />
-                        </div>
-                      );
-                    }}
-                  />
-                </div>
-              </div>
-              <div className={styles.informationSelect}>
-                <span className={styles.selectTitle}>Company size</span>
-                <div className={styles.selectInput}>
-                  <Controller
-                    name="size"
-                    control={control}
-                    render={({ field: { onChange, value } }) => {
-                      return (
-                        <div>
-                          <Select
-                            color="#1B1E27"
-                            onChange={onChange}
-                            objValue={value}
-                            data={size}
-                            placeholder="1 - 50"
-                          />
-                        </div>
-                      );
-                    }}
-                  />
-                </div>
-              </div>
-              <div className={styles.description}>
-                <span className={styles.descriptionTitle}>
-                  Company Description
-                </span>
-                <TextArea<CompanyEditFormTypes>
-                  register={register}
-                  name="companyDescription"
-                  placeholder="Describe the company's activities"
-                  error={errors.companyDescription}
-                />
-              </div>
-            </section>
-            <section className={styles.blockContacts}>
-              <h2 className={styles.contactsTitle}>Contacts</h2>
-              <div className={styles.contactsSelect}>
-                <span className={styles.selectTitle}>City</span>
-                <div className={styles.selectInput}>
-                  <Controller
-                    name="city"
-                    control={control}
-                    render={({ field: { onChange, value } }) => {
-                      return (
-                        <div>
-                          <Select
-                            color="#1B1E27"
-                            onChange={onChange}
-                            objValue={value}
-                            data={city}
-                            placeholder="Los-Angeles"
-                          />
-                        </div>
-                      );
-                    }}
-                  />
-                </div>
-              </div>
-              <div className={styles.contactsWrapperInput}>
-                <span className={styles.inputTitle}>Web site</span>
-                <div className={styles.contactsInput}>
-                  <Input<CompanyEditFormTypes>
-                    name="webSite"
-                    placeholder="stellar.org"
-                    register={register}
-                    error={errors.webSite}
-                  />
-                </div>
-              </div>
-              <div className={styles.contactsWrapperInput}>
-                <span className={styles.inputTitle}>Telegram</span>
-                <div className={styles.contactsInput}>
-                  <Input<CompanyEditFormTypes>
-                    name="telegram"
-                    placeholder="t.me/stellar"
-                    register={register}
-                    error={errors.telegram}
-                  />
-                </div>
-              </div>
-              {links.map((link, index) => (
-                <div key={link.id} className={styles.contactsWrapperInput}>
-                  <div className={styles.contactsInput}>
-                    <Input<CompanyEditFormTypes>
-                      name={`link-${index}`}
-                      placeholder="Paste link"
+    <>{
+      isWalletLoaded ? (
+        <form onSubmit={handleSubmit(onSubmit, error)} className={styles.canvas}>
+          <div className={styles.canvasWrapper}>
+            <div className={styles.wrapperBlockLinks}>
+              <Link className={styles.blockLink} href="/">
+                Main
+              </Link>
+              <span className={styles.blockSlash}>/</span>
+              <Link className={styles.blockLink} href="/companies">
+                Companies
+              </Link>
+              <span className={styles.blockSlash}>/</span>
+              <Link className={styles.blockLink} href="/company-edit">
+                Stellar
+              </Link>
+              <span className={styles.blockSlash}>/</span>
+              <Link className={styles.blockLinkCurrent} href="/company-edit">
+                Edit
+              </Link>
+            </div>
+            <h1 className={styles.blockTitle}>
+              <span className={styles.blockTitleText}>Editing</span>
+              <Button
+                appearance="primary"
+                size="l"
+                type="submit"
+                className={styles.buttonStyle}
+              >
+                Save changes
+              </Button>
+            </h1>
+            <div className={styles.wrapperBlockMain}>
+              <main className={styles.main}>
+                <section className={styles.blockBasicInformation}>
+                  <h2 className={styles.informationTitle}>Basic information</h2>
+                  <div className={styles.informationName}>
+                    <span className={styles.nameTitle}>Company name</span>
+                    <div className={styles.informationInput}>
+                      <Input<CompanyEditFormTypes>
+                        name="companyName"
+                        placeholder="Stellar"
+                        register={register}
+                        error={errors.companyName}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.informationSelect}>
+                    <span className={styles.selectTitle}>Industry</span>
+                    <div className={styles.selectInput}>
+                      <Controller
+                        name="industry"
+                        control={control}
+                        render={({ field: { onChange, value } }) => {
+                          return (
+                            <div>
+                              <Select
+                                color="#1B1E27"
+                                onChange={onChange}
+                                objValue={value}
+                                data={industry}
+                                placeholder="IT"
+                              />
+                            </div>
+                          );
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.informationSelect}>
+                    <span className={styles.selectTitle}>Company size</span>
+                    <div className={styles.selectInput}>
+                      <Controller
+                        name="size"
+                        control={control}
+                        render={({ field: { onChange, value } }) => {
+                          return (
+                            <div>
+                              <Select
+                                color="#1B1E27"
+                                onChange={onChange}
+                                objValue={value}
+                                data={size}
+                                placeholder="1 - 50"
+                              />
+                            </div>
+                          );
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.description}>
+                    <span className={styles.descriptionTitle}>
+                      Company Description
+                    </span>
+                    <TextArea<CompanyEditFormTypes>
                       register={register}
+                      name="companyDescription"
+                      placeholder="Describe the company's activities"
+                      error={errors.companyDescription}
                     />
                   </div>
+                </section>
+                <section className={styles.blockContacts}>
+                  <h2 className={styles.contactsTitle}>Contacts</h2>
+                  <div className={styles.contactsSelect}>
+                    <span className={styles.selectTitle}>City</span>
+                    <div className={styles.selectInput}>
+                      <Controller
+                        name="city"
+                        control={control}
+                        render={({ field: { onChange, value } }) => {
+                          return (
+                            <div>
+                              <Select
+                                color="#1B1E27"
+                                onChange={onChange}
+                                objValue={value}
+                                data={city}
+                                placeholder="Los-Angeles"
+                              />
+                            </div>
+                          );
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.contactsWrapperInput}>
+                    <span className={styles.inputTitle}>Web site</span>
+                    <div className={styles.contactsInput}>
+                      <Input<CompanyEditFormTypes>
+                        name="webSite"
+                        placeholder="stellar.org"
+                        register={register}
+                        error={errors.webSite}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.contactsWrapperInput}>
+                    <span className={styles.inputTitle}>Telegram</span>
+                    <div className={styles.contactsInput}>
+                      <Input<CompanyEditFormTypes>
+                        name="telegram"
+                        placeholder="t.me/stellar"
+                        register={register}
+                        error={errors.telegram}
+                      />
+                    </div>
+                  </div>
+                  {links.map((link, index) => (
+                    <div key={link.id} className={styles.contactsWrapperInput}>
+                      <div className={styles.contactsInput}>
+                        <Input<CompanyEditFormTypes>
+                          name={`link-${index}`}
+                          placeholder="Paste link"
+                          register={register}
+                        />
+                      </div>
+                    </div>
+                  ))}
+                  <div className={styles.contactsButton}>
+                    <Button
+                      onClick={addLink}
+                      size={"s"}
+                      appearance={"ghost"}
+                      iconPosition="left"
+                    >
+                      Add a link
+                    </Button>
+                  </div>
+                </section>
+              </main>
+              <aside className={styles.asideCreationLogo}>
+                {!acitveLogo ? (
+                  <Image
+                    className={styles.logoEmpty}
+                    src={LogoEmpty}
+                    priority
+                    alt="LogoEmpty"
+                  />
+                ) : (
+                  <Image
+                    src={linkLogoValue?.startsWith("https") ? linkLogoValue : "/"}
+                    width={320}
+                    height={320}
+                    alt="Error loading image"
+                  />
+                )}
+                <span className={styles.creationLogoText}>Image link</span>
+                <div className={styles.creationLogoInput}>
+                  <Input<CompanyEditFormTypes>
+                    name="linkLogo"
+                    placeholder="Insert link"
+                    register={register}
+                    error={errors.linkLogo}
+                  />
+                  <Image
+                    onClick={() => changeActiveQuestion()}
+                    src={Question}
+                    priority
+                    alt={"Question"}
+                    className={styles.inputQuestion}
+                  />
+                  <div
+                    className={cn(styles.infoQuestion, {
+                      [styles.questionActive]: activeQuestion,
+                    })}
+                  >
+                    The logo image must have a width and height of 320 px
+                  </div>
                 </div>
-              ))}
-              <div className={styles.contactsButton}>
-                <Button
-                  onClick={addLink}
-                  size={"s"}
-                  appearance={"ghost"}
-                  iconPosition="left"
-                >
-                  Add a link
-                </Button>
-              </div>
-            </section>
-          </main>
-          <aside className={styles.asideCreationLogo}>
-            {!acitveLogo ? (
-              <Image
-                className={styles.logoEmpty}
-                src={LogoEmpty}
-                priority
-                alt="LogoEmpty"
-              />
-            ) : (
-              <Image
-                src={linkLogoValue?.startsWith("https") ? linkLogoValue : "/"}
-                width={320}
-                height={320}
-                alt="Error loading image"
-              />
-            )}
-            <span className={styles.creationLogoText}>Image link</span>
-            <div className={styles.creationLogoInput}>
-              <Input<CompanyEditFormTypes>
-                name="linkLogo"
-                placeholder="Insert link"
-                register={register}
-                error={errors.linkLogo}
-              />
-              <Image
-                onClick={() => changeActiveQuestion()}
-                src={Question}
-                priority
-                alt={"Question"}
-                className={styles.inputQuestion}
-              />
-              <div
-                className={cn(styles.infoQuestion, {
-                  [styles.questionActive]: activeQuestion,
-                })}
-              >
-                The logo image must have a width and height of 320 px
-              </div>
+              </aside>
             </div>
-          </aside>
-        </div>
-      </div>
-    </form>
+          </div>
+        </form>
+      ) : (
+        <div className={styles.stub}>Wallet is loading, please wait...</div>
+      )
+    }</>
+
   );
 };
 
