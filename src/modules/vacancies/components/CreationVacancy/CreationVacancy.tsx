8c5e28708358260523f9fcd531e5b1043afa7c9d
JoDcntrl <jodcntrl28@gmail.com>
2024-05-23 13:41:57 +0300

    DCNTRL-18 feat added a transition to the main page if there is no wallet

--- src/modules/vacancies/components/CreationVacancy/CreationVacancy.tsx
+++ src/modules/vacancies/components/CreationVacancy/CreationVacancy.tsx
@@ -1,27 +1,57 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
+import { useRouter } from "next/navigation";
+import { useIsConnectionRestored, useTonWallet } from '@tonconnect/ui-react';
 import { VacancyForm } from "@/modules/vacancies/components/vacancyForm/VacancyForm";
 
 import styles from "./styles.module.scss";
 
-export const CreationVacancy = () => (
-  <div className={styles.creation}>
-    <div className={styles.creationWrapper}>
-      <div className={styles.wrapperBlockLinks}>
-        <Link className={styles.blockLink} href="/">
-          Main
-        </Link>
-        <span className={styles.blockSlash}>/</span>
-        <Link className={styles.blockLink} href="/companies">
-          Companies
-        </Link>
-        <span className={styles.blockSlash}>/</span>
-        <Link className={styles.blockLinkCurrent} href={""}>
-          Stellar
-        </Link>
+export const CreationVacancy = () => {
+
+  const [isWalletLoaded, setIsWalletLoaded] = useState<boolean>(false);
+
+  const wallet = useTonWallet();
+  const router = useRouter();
+  const connectionRestored = useIsConnectionRestored();
+
+  useEffect(() => {
+    if(connectionRestored) {
+      if(!wallet) {
+        router.push('/')
+      } else {
+        setIsWalletLoaded(true)
+      }
+    }    
+  },[connectionRestored,wallet]);
+
+  return (
+    <>{
+      isWalletLoaded ? (
+        <div className={styles.creation}>
+        <div className={styles.creationWrapper}>
+          <div className={styles.wrapperBlockLinks}>
+            <Link className={styles.blockLink} href="/">
+              Main
+            </Link>
+            <span className={styles.blockSlash}>/</span>
+            <Link className={styles.blockLink} href="/companies">
+              Companies
+            </Link>
+            <span className={styles.blockSlash}>/</span>
+            <Link className={styles.blockLinkCurrent} href={""}>
+              Stellar
+            </Link>
+          </div>
+          <h1 className={styles.title}>Job creation</h1>
+          <VacancyForm />
+        </div>
       </div>
-      <h1 className={styles.title}>Job creation</h1>
-      <VacancyForm />
-    </div>
-  </div>
-);
+      ) : (
+        <div className={styles.stub}></div>
+      )
+    }</>
+
+)};
