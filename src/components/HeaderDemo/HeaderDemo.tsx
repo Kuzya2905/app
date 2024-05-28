1718921685673edf3f08cd74c2e35c8099c8da68
JoDcntrl <jodcntrl28@gmail.com>
2024-05-28 17:19:49 +0300

    DCNTRL-25 feat Added a button Connect wallet

--- src/components/HeaderDemo/HeaderDemo.tsx
+++ src/components/HeaderDemo/HeaderDemo.tsx
@@ -3,11 +3,12 @@
 import React, { useState, useEffect } from "react";
 import cn from "classnames";
 import LinkNext from "next/link";
-import { TonConnectButton } from "@tonconnect/ui-react";
+import { TonConnectButton, useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
 
 import Link from "@/components/Link/Link";
 import { LinksArr } from "./HeaderData";
 import { HeaderDemoTypes } from "@/components/HeaderDemo/Header.types";
+import Button from "../Button/Button";
 
 import { Logo } from "@/assets/svgs/Logo";
 import { Burger } from "@/assets/svgs/Burger";
@@ -15,8 +16,11 @@ import { Cross } from "@/assets/svgs/Cross";
 
 import styles from "./headerDemo.module.scss";
 
+
 const HeaderDemo: React.FC<HeaderDemoTypes> = ({ className, ...props }) => {
   const [activeBurger, setActiveBurger] = useState(false);
+  const [tonConnectUi] = useTonConnectUI();
+  const wallet = useTonWallet();
 
   const toggleBurger = () => {
     setActiveBurger((prev) => !prev);
@@ -62,7 +66,17 @@ const HeaderDemo: React.FC<HeaderDemoTypes> = ({ className, ...props }) => {
             ))}
           </div>
           <div className={styles.buttonConnectWalletWrapper}>
-            <TonConnectButton />
+            <Button
+              appearance="secondary"
+              size="l"
+              type="submit"
+              onClick={()=> {
+                //tonConnectUi.openModal()
+                //console.log(wallet)
+              }}
+            > 
+              Connect wallet</Button>
+            <TonConnectButton /> 
           </div>
         </div>
         {activeBurger ? (
